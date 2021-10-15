594ea9f62eb19b0cbbeaa5d37e8b181ea2ab3953
Simple Dimple John <simpledimplejohn@gmail.com>
2021-10-15 12:30:36 -0700

    add primatives to BasicJava.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,5 @@
 package com.company;
+import com.company.examples.BasicJava;
 import com.company.examples.InterfaceExample;
 import com.company.examples.LoopsExamples;
 import com.company.examples.Sketch;
@@ -10,14 +11,14 @@ public class Main {
 
         //Interface Example is the number 3 test, built by Lewis and John
         InterfaceExample interFaceExample = new InterfaceExample();
-        Sketch newSketch = new Sketch();
-        newSketch.forLoop();
-
 //        System.out.println(interFaceExample.isThree(2) + " [false]");
 //        System.out.println(interFaceExample.isThree(3) + " [true]");
 //        System.out.println(interFaceExample.isThree(4) + " [false]")
 //        System.out.println(interFaceExample.isTwo(0));
-
+        Sketch newSketch = new Sketch();
+        //newSketch.forLoop();
+        BasicJava newBasicJava = new BasicJava();
+        newBasicJava.printBasics();
 
     }
 }
--- src/com/company/examples/BasicJava.java
+++ src/com/company/examples/BasicJava.java
@@ -1,7 +1,26 @@
 package com.company.examples;
 
 public class BasicJava {
-    public void testFunction() {
+    byte firstByte = 12;
+    short firstShort = 32_000;
+    int intNum = 69;
+    long longNum = 900_000_000;
+    float floatNum = 900;
+    double doubleNum = 69.96;
+    boolean boolVal = true;
+    char charVal = 'a';
+    String newString = "works";
+
+    public void printBasics() {
+        System.out.println("byte size 8: " + firstByte);
+        System.out.println("short size 16: " + firstShort);
+        System.out.println("int size 32: " + intNum);
+        System.out.println("long size 64: " + longNum);
+        System.out.println("float size 32: " + floatNum);
+        System.out.println("double size 64 " + doubleNum);
+        System.out.println(("boolean size 1: " + boolVal));
+        System.out.println("char size 15: " + charVal);
+        System.out.println("string " + newString);
 
     }
 }
